70c2caae1c0a1cfd7d5b02748df6aa28f6b5ef81
kishor-kumar-parida <126803569+kishor-kumar-parida@users.noreply.github.com>
2023-08-16 22:05:53 +0530

    Initial commit

---
+++ src/components/App.jsx
@@ -0,0 +1,23 @@
+import React, { useState } from "react";
+
+function App() {
+  setInterval(updateTime, 1000);
+
+  const now = new Date().toLocaleTimeString();
+
+  const [time, setTime] = useState(now);
+
+  function updateTime() {
+    const newTime = new Date().toLocaleTimeString();
+    setTime(newTime);
+  }
+
+  return (
+    <div className="container">
+      <button onClick={updateTime}>Time</button>
+      <h1>{time}</h1>
+    </div>
+  );
+}
+
+export default App;
